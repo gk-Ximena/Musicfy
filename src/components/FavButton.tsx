82ec92312c5a5f678dd87f4043660e4f83db481c
Ximena <xalvarezmontoy00@mylangara.ca>
2026-08-17 00:30:01 -0700

    Bug fixed for command line when deploying app

--- src/components/FavButton.tsx
+++ src/components/FavButton.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./../styles/FavButton.css";
 
 export default function FavButton() {
-    //Animate button when is being pressed
-    const [isPressed, setIsPressed] = useState(false);
 
     return (
         <div className="fav-button-background">
